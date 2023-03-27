5471e536509dc47c17ca4a230ebca86c417f5930
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-03-26 21:24:40 -0600

    commit tree f3722f218689deb29da59841822fee0c2a3d3fa0

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useEffect, useState}  from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+const [plants, setPlants] = useState([])
+const [search, setSearch] = useState('')
+
+const filteredPlants = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+  useEffect(()=> {
+    fetch ("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(setPlants)
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
